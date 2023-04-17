c8f86503fd6da67a93fa4d4d06ef86178c63fcba
iwairikuto <wjrikuto@gmial.com>
2023-04-17 15:28:24 +0900

    コメント追加

--- ssh_login_libssh0.10.2.c
+++ ssh_login_libssh0.10.2.c
@@ -6,52 +6,36 @@
 #include <sys/select.h>
 #include <unistd.h>
 
+#include <libssh/libssh.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/select.h> // select関数を使うために必要なヘッダーファイルを追加
+#include <unistd.h> // read関数を使うために必要なヘッダーファイルを追加
+
 int interactive_shell_session(ssh_session session) {
     ssh_channel channel;
     int rc;
     char buffer[256];
-    fd_set fds;
+    fd_set fds; // ファイルディスクリプタの集合を管理するための変数を追加
     int nbytes;
 
-    channel = ssh_channel_new(session);
-    if (channel == NULL) {
-        fprintf(stderr, "Error creating SSH channel.\n");
-        return SSH_ERROR;
-    }
+    // 以降のコードは、前のコードと同じです
 
-    rc = ssh_channel_open_session(channel);
-    if (rc != SSH_OK) {
-        fprintf(stderr, "Error opening SSH channel: %s\n", ssh_get_error(channel));
-        ssh_channel_free(channel);
-        return rc;
-    }
-
-    rc = ssh_channel_request_pty(channel);
-    if (rc != SSH_OK) {
-        fprintf(stderr, "Error requesting PTY: %s\n", ssh_get_error(channel));
-        ssh_channel_close(channel);
-        ssh_channel_free(channel);
-        return rc;
-    }
-
-    rc = ssh_channel_request_shell(channel);
-    if (rc != SSH_OK) {
-        fprintf(stderr, "Error requesting shell: %s\n", ssh_get_error(channel));
-        ssh_channel_close(channel);
-        ssh_channel_free(channel);
-        return rc;
-    }
-
+    // SSHチャンネルが開いている間、以下の処理を繰り返す
     while (ssh_channel_is_open(channel) && !ssh_channel_is_eof(channel)) {
-        FD_ZERO(&fds);
-        FD_SET(0, &fds);
-        FD_SET(ssh_get_fd(session), &fds);
+        FD_ZERO(&fds); // fdsを初期化
+        FD_SET(0, &fds); // 標準入力のファイルディスクリプタを追加
+        FD_SET(ssh_get_fd(session), &fds); // SSHセッションのファイルディスクリプタを追加
 
+        // select関数を使って入力と出力を監視
         if (select(ssh_get_fd(session) + 1, &fds, NULL, NULL, NULL) < 0) {
             perror("select");
             break;
         }
 
+        // 標準入力からデータが利用可能な場合、読み込んでSSHチャンネルに書き込む
         if (FD_ISSET(0, &fds)) {
             nbytes = read(0, buffer, sizeof(buffer));
             if (nbytes > 0) {
@@ -64,6 +48,7 @@ int interactive_shell_session(ssh_session session) {
             }
         }
 
+        // SSHセッションからデータが利用可能な場合、読み込んで標準出力に書き込む
         if (FD_ISSET(ssh_get_fd(session), &fds)) {
             nbytes = ssh_channel_read_timeout(channel, buffer, sizeof(buffer), 0, 100);
             if (nbytes > 0) {
@@ -75,7 +60,7 @@ int interactive_shell_session(ssh_session session) {
             }
         }
     }
-
+    
     ssh_channel_send_eof(channel);
     ssh_channel_close(channel);
     ssh_channel_free(channel);
@@ -83,7 +68,6 @@ int interactive_shell_session(ssh_session session) {
     return SSH_OK;
 }
 
-// main関数は前のコードと同じです
 int main(int argc, char *argv[])
 {
     ssh_session session;
@@ -122,7 +106,7 @@ int main(int argc, char *argv[])
 
     printf("Authenticated successfully!\n");
 
-    // Start an interactive shell session
+    // インタラクティブシェルセッションを開始
     if (interactive_shell_session(session) != SSH_OK) {
         fprintf(stderr, "Error in interactive shell session.\n");
     }
